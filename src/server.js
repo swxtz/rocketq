3c760d8f832f404480a9005b3eb5a2d9e19e6d6b
swxtz <alf4r6@gmail.com>
2022-10-16 18:38:14 -0300

    refactor: move css to public and convert html to ejs

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
-const { application } = require("express");
 const express = require("express");
 const route = require("./routes");
 const path = require("path")
+const port = process.env.PORT || 3333;
 
 const server = express();
 
 server.set("view engine", "ejs");
+server.use(express.static("public"));
 server.set("views", path.join(__dirname, "views"));
 
 server.use(route);
 
-server.listen("3333", () => {
-  console.log("rodando");
+server.listen(port, () => {
+  console.log(`🚀 Rodando servidor em http://localhost:${port}`);
 });
