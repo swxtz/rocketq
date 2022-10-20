177a54ed786f544e34c2bdd95cb172b23ece6273
swxtz <alf4r6@gmail.com>
2022-10-20 09:46:46 -0300

    feat: add lister for delete button

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,18 @@ const modal = Modal();
 
 //Verificar quando o usuario abrir o modal
 const checkButtons = document.querySelectorAll(".actions a.check");
+
 checkButtons.forEach(button => {
   button.addEventListener("click", event => {
     modal.open();
   });
 });
 
+//Verificar quando o usuario clicar no botão de deletar
+const deleteButton = document.querySelectorAll(".actions a.delete");
 
+deleteButton.forEach(button =>  {
+  button.addEventListener("click", event => {
+    modal.open()
+  });
+});
