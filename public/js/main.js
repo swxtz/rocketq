9217a41474723f545cc59d222dc1eff676170f52
swxtz <alf4r6@gmail.com>
2022-10-20 16:45:41 -0300

    feat: create room controller

--- public/js/main.js
+++ public/js/main.js
@@ -28,7 +28,7 @@ function handleClick(event, check = true) {
   
   
   const form = document.querySelector(".modal form");
-  form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`);
+  form.setAttribute("action", `/question/${roomId}/${questionId}/${slug}`);
 
   modalTitle.innerHTML= check ? "Marcar como lida esta pergunta" : "Excluir esta pergunta";
 
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const questionController = require("./controllers/questionController");
+const roomController = require("./controllers/roomController");
 
 const route = express.Router();
 
 route.get("/", (req, res) => {
-  res.render("index")
+  res.render("index", {page: "enter-room"});
 });
 
-route.get("/room", (req, res) => {
-  res.render("room")
+route.get("/room/:room", (req, res) => {
+  res.render("room");
 });
 
 route.get("/create-pass", (req, res) => {
-  res.render("create-pass")
+  res.render("index", {page: "create-pass"});
 });
 
-route.post("/room/:room/:question/:action", questionController.index)
+route.post("/question/:room/:question/:action", questionController.index);
+route.post("/create-room", roomController.create);
 
 module.exports = route;
