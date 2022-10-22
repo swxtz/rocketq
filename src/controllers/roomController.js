9b69796e3c87bc3afc15b311c0b9003b9ea2b0bb
swxtz <alf4r6@gmail.com>
2022-10-22 12:56:01 -0300

    feat: dynamic questions

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -30,8 +30,13 @@ module.exports = {
     res.redirect(`/room/${roomId}`);
   },
 
-  open(req, res) {
-   const roomId = req.params.room
-    res.render("room", {roomId: roomId})
+  async open(req, res) {
+    const db = await Database();
+    const roomId = req.params.room
+    const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} AND read = 0`);
+    const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} AND read = 1`);
+
+    res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead})
+
   }
 };
