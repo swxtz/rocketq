28591770734348888314143eab6df4e253694364
swxtz <alf4r6@gmail.com>
2022-10-22 11:15:09 -0300

    fix(db): bug fix when importing db
    fixed bug where db import is not working causing server to crash
    
    BREAKING CHANGE: due to bug it will break any old import

--- src/db/init.js
+++ src/db/init.js
@@ -1,22 +1,23 @@
-const Database = require("./config");
+const Database = require("./config")
 
 const initDb = {
-  async init() {
-    const db = await Database();
+    async init(){
+        const db = await Database()
 
-    await db.exec(`CREATE TABLE rooms (
-      id INTEGER PRIMATY KEY,
-      pass TEXT
-    )`);
+        await db.exec(`CREATE TABLE rooms (
+            id INTEGER PRIMARY KEY,
+            pass TEXT
+        )`);
 
-    await db.exec(`CREATE TABLE questions (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT,
-      read INT,
-    )`);
-        
-    await db.close();
-  }
-};
+        await db.exec(`CREATE TABLE questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            read INT
+        )`);
+
+        await db.close()
+    }
+}
 
 initDb.init();
+
